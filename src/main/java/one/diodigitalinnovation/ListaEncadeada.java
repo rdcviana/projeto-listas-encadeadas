51f4e939da0fe99b14abb329c8352ee9abb228be
Rodrigo Viana <rodrigodcviana@gmail.com>
2023-03-30 15:47:15 -0300

    Commit com a implementação dos métodos add() e getNo() da classe ListaEncadeada.java

--- src/main/java/one/diodigitalinnovation/ListaEncadeada.java
+++ src/main/java/one/diodigitalinnovation/ListaEncadeada.java
@@ -8,6 +8,40 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+    private void validaIndice(int index) {
+        if (index <= size()) {
+            int ultimoIndice = size() - 1;
+            throw new IndexOutOfBoundsException("Não existe conteúdo no índice " + index + " desta lista." +
+                    " Esta lista só vai até o índice " + ultimoIndice);
+        }
+    }
+
+    private No<T> getNo(int index) {
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+        for (int i = 0; i <= this.size() - 1; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
+    public void add(No conteudo) {
+        No<T> novoNo = new No<>(conteudo);
+        if (this.isEmpty()) {
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for (int i = 0; i <= this.size() - 1; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
     public int size() {
         int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
