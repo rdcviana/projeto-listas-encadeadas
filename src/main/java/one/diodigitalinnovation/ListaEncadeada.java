aa0209da1c0715e57deb85c2519fd647fabb0c8e
Rodrigo Viana <rodrigodcviana@gmail.com>
2023-03-30 15:02:26 -0300

    Commit com a implementação da Classe No e ListaEncadeada

---
+++ src/main/java/one/diodigitalinnovation/ListaEncadeada.java
@@ -0,0 +1,30 @@
+package one.diodigitalinnovation;
+
+public class ListaEncadeada<T> {
+
+    No<T> referenciaEntrada = null;
+
+    public ListaEncadeada() {
+        this.referenciaEntrada = null;
+    }
+
+    public int size() {
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+        while (true) {
+            if (referenciaEntrada != null) {
+                tamanhoLista++;
+                if (referenciaAux.getProximoNo() != null) {
+                    referenciaAux = referenciaAux.getProximoNo();
+                } else {
+                    break;
+                }
+            }
+        }
+        return tamanhoLista;
+    }
+
+    public boolean isEmpty() {
+        return referenciaEntrada == null ? true : false;
+    }
+}
